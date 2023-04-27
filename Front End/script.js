4b380ee228e4e6a99c79c7bd8d4528bc4ca3eab8
Marshall Peters <mj_peters@btconnect.com>
2023-04-27 17:50:55 +0100

    Added links for each token we are using

--- Front End/script.js
+++ Front End/script.js
@@ -1,11 +1,9 @@
-// Listen for a click, then change the network icon to the corresponding image
+// Listen for a click to change the current icon shown on the dropdown menu
+// Network dropdown menu
 const dropdownContent = document.querySelector(".dropdown-content");
-const inCoinDropdownContent = document.querySelector(".inCoin-dropdown-content");
 const networkIcon = document.querySelector("#network-icon");
-const inCoinIcon = document.querySelector("#inCoin-icon");
 const svgIcon = document.querySelector("#network-icon + svg");
-const inCoinSvgIcon = document.querySelector("#inCoin-icon + svg");
-          
+
 dropdownContent.addEventListener("click", function(event) {
     if (event.target.tagName === "A") {
         const network = event.target.getAttribute('data-network');
@@ -14,6 +12,11 @@ dropdownContent.addEventListener("click", function(event) {
         svgIcon.classList.remove("rotate");
     }
 });
+
+// inCoin dropdown menu
+const inCoinDropdownContent = document.querySelector(".inCoin-dropdown-content");
+const inCoinIcon = document.querySelector("#inCoin-icon");
+const inCoinSvgIcon = document.querySelector("#inCoin-icon + svg");
 
 inCoinDropdownContent.addEventListener("click", function(event) {
     if (event.target.tagName === "A") {
@@ -24,18 +27,44 @@ inCoinDropdownContent.addEventListener("click", function(event) {
     }
 });
 
+// outCoin dropdown menu
+const outCoinDropdownContent = document.querySelector(".outCoin-dropdown-content");
+const outCoinIcon = document.querySelector("#outCoin-icon");
+const outCoinSvgIcon = document.querySelector("#outCoin-icon + svg");
+
+outCoinDropdownContent.addEventListener("click", function(event) {
+    if (event.target.tagName === "A") {
+        const outCoin = event.target.getAttribute('data-network');
+        const outCoinUrl = event.target.querySelector(".outCoin-dropdown-icon").getAttribute("src");
+        outCoinIcon.setAttribute("src", outCoinUrl);
+        outCoinSvgIcon.classList.remove("rotate");
+    }
+});
+
 // Listen for a click to show the dropdown menu and rotate the SVG icon
+// Network dropdown rotation
 const dropdownButton = document.querySelector('.change-network');
 const dropdownMenu = document.querySelector('.dropdown-content');
-const inCoinDropdownButton = document.querySelector('.change-inCoin');
-const inCoinDropdownMenu = document.querySelector('.inCoin-dropdown-content');
 
 dropdownButton.addEventListener('click', function() {
   dropdownMenu.classList.toggle('show');
   svgIcon.classList.toggle('rotate');
 });
 
+// inCoin dropdown rotation
+const inCoinDropdownButton = document.querySelector('.change-inCoin');
+const inCoinDropdownMenu = document.querySelector('.inCoin-dropdown-content');
+
 inCoinDropdownButton.addEventListener('click', function() {
     inCoinDropdownMenu.classList.toggle('show');
     inCoinSvgIcon.classList.toggle('rotate');
+});
+
+// outCoin dropdown rotation
+const outCoinDropdownButton = document.querySelector('.change-outCoin');
+const outCoinDropdownMenu = document.querySelector('.outCoin-dropdown-content');
+
+outCoinDropdownButton.addEventListener('click', function() {
+    outCoinDropdownMenu.classList.toggle('show');
+    outCoinSvgIcon.classList.toggle('rotate');
 });
