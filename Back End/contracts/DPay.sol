4d2c9b3a9b13c46d536806a022126efe5b02ed49
Edward <83250017+rw19842@users.noreply.github.com>
2023-05-11 17:07:24 +0100

    Moved back to using SwapRouter
    see https://github.com/rw19842/DPay-proof-of-concept/issues/7#issuecomment-1544269192

--- Back End/contracts/DPay.sol
+++ Back End/contracts/DPay.sol
@@ -1,171 +1,91 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.0;
 
-import "https://github.com/Uniswap/v3-periphery/blob/main/contracts/libraries/TransferHelper.sol";
-import "https://github.com/Uniswap/universal-router/blob/main/contracts/interfaces/IUniversalRouter.sol";
-import "https://github.com/Uniswap/permit2/blob/main/src/interfaces/IAllowanceTransfer.sol";
-import {SafeTransferLib} from "https://github.com/transmissions11/solmate/blob/main/src/utils/SafeTransferLib.sol";
-import {Constants} from "https://github.com/Uniswap/universal-router/blob/main/contracts/libraries/Constants.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "./IWETH.sol";
+
 
 contract DPay {
 
-    // see https://github.com/Uniswap/universal-router/tree/main/deploy-addresses for address
-    IUniversalRouter public constant universalRouter = IUniversalRouter(0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD);
-
-    // setting WETH contract address
-    address internal constant weth9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-
-    // set the pool fee to 0.3%
-    uint24 internal constant poolFee = 3000;
-
-    function swapSend(
-        address outCoin,
-        uint256 outAmount,
+    ISwapRouter private constant swapRouter = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+    address private constant weth9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    
+    function swapTokenSendToken(
+        address outCoin, 
+        uint256 outAmount, 
         address inCoin, 
-        uint160 maxInAmount,
-        bytes calldata _signature,
-        uint48 _expiration,
-        uint48 _nonce,
+        uint256 maxInAmount,
         address recipient
-    ) external {
+    ) external returns(uint256 inAmount) {
 
-        // defining inputs for universalRouter.execute()
-        bytes memory commands = new bytes(2);
-        commands[0] = 0x0a; // PERMIT2_PERMIT
-        commands[1] = 0x01; // V3_SWAP_EXACT_OUT
-        bytes[] memory inputs = new bytes[](2);
+        // sending inCoin to DPay
+        TransferHelper.safeTransferFrom(inCoin, msg.sender, address(this), maxInAmount);
+    
+        // approving inCoin for swapRouter
+        TransferHelper.safeApprove(inCoin, address(swapRouter), maxInAmount);
 
-        // using Permit2 to permit universalRouter to spend users tokens
-        IAllowanceTransfer.PermitDetails memory permitDetails = 
-            IAllowanceTransfer.PermitDetails({
-                token: inCoin,
-                amount: maxInAmount,
-                expiration: _expiration,
-                nonce: _nonce
+        // parameters for swapRouter
+        ISwapRouter.ExactOutputSingleParams memory params =
+            ISwapRouter.ExactOutputSingleParams({
+                tokenIn: inCoin,
+                tokenOut: outCoin,
+                fee: 3000, // 0.3%
+                recipient: recipient,
+                deadline: type(uint256).max,
+                amountOut: outAmount,
+                amountInMaximum: maxInAmount,
+                sqrtPriceLimitX96: 0
             });
+        
+        // ammount of inCoin which got spent
+        inAmount = swapRouter.exactOutputSingle(params);
 
-        IAllowanceTransfer.PermitSingle memory permitSingle = 
-            IAllowanceTransfer.PermitSingle({
-                details: permitDetails,
-                spender: address(universalRouter),
-                sigDeadline: _expiration
-            });
+        // revoking permission given to swapRouter
+        TransferHelper.safeApprove(inCoin, address(swapRouter), 0);
 
-        // inputs for PERMIT2_PERMIT
-        inputs[0] = abi.encode(permitSingle, _signature);
+        // refunding excess inCoin
+        if (inAmount < maxInAmount) {
+            TransferHelper.safeTransfer(inCoin, msg.sender, maxInAmount - inAmount);
+        }
 
-        // inputs for V3_SWAP_EXACT_OUT
-        // swapping inCoin for outCoin and sending it to recipient
-        inputs[1] = abi.encode(
-            recipient,
-            outAmount,
-            maxInAmount,
-            abi.encodePacked(outCoin, poolFee, inCoin),
-            true // value for payerIsUser
-        );
-
-        // executing the swap
-        universalRouter.execute(commands, inputs, block.timestamp);
     }
 
-    function swapEthSend(
+    function swapEthSendToken(
         address outCoin,
         uint256 outAmount,
         address recipient
     ) external payable {
 
-        // sending all ETH from transaction to universalRouter
-        SafeTransferLib.safeTransferETH(address(universalRouter), msg.value);
+        // wrapping users ETH
+        IWETH(weth9).deposit{value: msg.value}();
 
-        // defining inputs for universalRouter.execute()
-        bytes memory commands = new bytes(3);
-        commands[0] = 0x0b; // WRAP_ETH
-        commands[1] = 0x01; // V3_SWAP_EXACT_OUT
-        commands[2] = 0x0c; // UNWRAP_ETH
-        bytes[] memory inputs = new bytes[](3);
+        // approving weth9 for swapRouter
+        TransferHelper.safeApprove(weth9, address(swapRouter), msg.value);
 
-        // inputs for WRAP_ETH
-        // wrapping all eth on univeralRouter, sending WETH to univeralRouter
-        inputs[0] = abi.encode(
-            address(universalRouter),
-            Constants.CONTRACT_BALANCE
-        );
-
-        // inputs for V3_SWAP_EXACT_OUT
-        // swapping WETH for outCoin and sending it to recipient
-        inputs[1] = abi.encode(
-            recipient,
-            outAmount,
-            IERC20(weth9).balanceOf(address(universalRouter)),
-            abi.encodePacked(outCoin, poolFee, weth9),
-            false // value for payerIsUser
-        );
-
-        // inputs for UNWRAP_ETH
-        // un-wrapping all remaining WETH and sending it to msg.sender
-        inputs[2] = abi.encode(
-            msg.sender,
-            1
-        );
-
-        // executing the swap
-        universalRouter.execute(commands, inputs, block.timestamp);
-    }
-
-    function swapSendEth(
-        uint256 outAmount,
-        address inCoin,
-        uint160 maxInAmount,
-        bytes calldata _signature,
-        uint48 _expiration,
-        uint48 _nonce,
-        address recipient
-    ) external {
-
-        // defining inputs for universalRouter.execute()
-        bytes memory commands = new bytes(3);
-        commands[0] = 0x0a; // PERMIT2_PERMIT
-        commands[1] = 0x01; // V3_SWAP_EXACT_OUT
-        commands[2] = 0x0c; // UNWRAP_ETH
-        bytes[] memory inputs = new bytes[](3);
-
-        // using Permit2 to permit universalRouter to spend users tokens
-        IAllowanceTransfer.PermitDetails memory permitDetails = 
-            IAllowanceTransfer.PermitDetails({
-                token: inCoin,
-                amount: maxInAmount,
-                expiration: _expiration,
-                nonce: _nonce
+        // parameters for swapRouter
+        ISwapRouter.ExactOutputSingleParams memory params =
+            ISwapRouter.ExactOutputSingleParams({
+                tokenIn: weth9,
+                tokenOut: outCoin,
+                fee: 3000, // 0.3%
+                recipient: recipient,
+                deadline: type(uint256).max,
+                amountOut: outAmount,
+                amountInMaximum: msg.value,
+                sqrtPriceLimitX96: 0
             });
 
-        IAllowanceTransfer.PermitSingle memory permitSingle = 
-            IAllowanceTransfer.PermitSingle({
-                details: permitDetails,
-                spender: address(universalRouter),
-                sigDeadline: _expiration
-            });
+        // ammount of inCoin which got spent
+        uint256 inAmount = swapRouter.exactOutputSingle(params);
 
-        // inputs for PERMIT2_PERMIT
-        inputs[0] = abi.encode(permitSingle, _signature);
+        // revoking permission given to swapRouter
+        TransferHelper.safeApprove(weth9, address(swapRouter), 0);
 
-        // inputs for V3_SWAP_EXACT_OUT
-        // swapping inCoin for WETH and sending it to univeralRouter
-        inputs[1] = abi.encode(
-            address(universalRouter),
-            outAmount,
-            maxInAmount,
-            abi.encodePacked(weth9, poolFee, inCoin),
-            true // value for payerIsUser
-        );
-
-        // inputs for UNWRAP_ETH
-        // un-wrapping all WETH and sending it to recipient
-        inputs[2] = abi.encode(
-            recipient,
-            1
-        );
-
-        // executing the swap
-        universalRouter.execute(commands, inputs, block.timestamp);
+        // refunding excess WETH
+        if (inAmount < msg.value) {
+            TransferHelper.safeTransfer(weth9, msg.sender, IERC20(weth9).balanceOf(address(this)));
+        }
     }
 }
