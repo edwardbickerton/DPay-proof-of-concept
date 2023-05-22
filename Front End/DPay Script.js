1d156560314a56bfa12e131db9abacad001fdec4
Marshall Peters <mj_peters@btconnect.com>
2023-05-22 12:25:37 +0100

    fixed connect button bug

--- Front End/DPay Script.js
+++ Front End/DPay Script.js
@@ -129,14 +129,22 @@ function getChainId(network) {
     }
 }
 
-/* Connect metamask account */
+// Connect metamask account
 async function connect() {
-    if (typeof window.ethereum !== "undefined") {
-        await ethereum.request({method: "eth_requestAccounts"})
-    } else {
-        alert("You haven't get metamask installed, please install it on the web site!")
+    try {
+        if (typeof window.ethereum !== "undefined") {
+        await ethereum.request({ method: "eth_requestAccounts" });
+        } else {
+        throw new Error("MetaMask is not installed.");
+        }
+    } catch (error) {
+        alert(error.message);
     }
 }
+
+// Add event listener to the "Connect" button
+const connectButton = document.getElementById("connectButton");
+connectButton.addEventListener("click", connect);
 
 // Defining variables for quantity calculations
 let selectedInCoin = "";
@@ -149,86 +157,99 @@ let outCoinPrice;
 
 // Function to calculate the inCoin quantity based on the specified outCoin quantity
 function calculateInCoinQuantity(outCoinQuantity) {
-  return (outCoinQuantity * outCoinPrice * 1.1) / inCoinPrice;
+    return (outCoinQuantity * outCoinPrice * 1.1) / inCoinPrice;
 }
 
 // Add event listener to inCoin dropdown button
 const inDropdownBtn = document.querySelector(".inCoin-dropdown-content");
-inDropdownBtn.addEventListener("click", function(event) {
-  // Get the selected coin from the clicked dropdown item
-  if (event.target.tagName === "A") {
-    selectedInCoin = event.target.getAttribute("data-inCoin");
-    selectedInSymbol = event.target.getAttribute("data-inSymbol");
-    let apiUrl = `https://api.coingecko.com/api/v3/simple/price?ids=${selectedInCoin}&vs_currencies=GBP`;
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        inCoinPrice = data[selectedInCoin].gbp;
-        console.log(inCoinPrice); // This will print the price in GBP to the console
+inDropdownBtn.addEventListener("click", function (event) {
+    // Get the selected coin from the clicked dropdown item
+    if (event.target.tagName === "A") {
+        selectedInCoin = event.target.getAttribute("data-inCoin");
+        selectedInSymbol = event.target.getAttribute("data-inSymbol");
+        let apiUrl = `https://api.coingecko.com/api/v3/simple/price?ids=${selectedInCoin}&vs_currencies=GBP`;
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                inCoinPrice = data[selectedInCoin].gbp;
+                console.log(inCoinPrice); // This will print the price in GBP to the console
 
-        // Calculate the inCoin quantity if the outCoin price is available
-        if (outCoinPrice) {
-          const outCoinQuantity = parseFloat(document.querySelector("#outCoin-transfer-quantity").value);
-          const inCoinQuantity = calculateInCoinQuantity(outCoinQuantity);
-          const quantityLabel = document.querySelector("#inCoin-quantity-label #inCoin-transfer-quantity");
-          if (!isNaN(inCoinQuantity)) {
-            quantityLabel.textContent = `${inCoinQuantity.toFixed(6)} ${selectedInSymbol}`;
-          } else {
-            quantityLabel.textContent = "";
-          }
-        }
-      })
-      .catch(error => console.error(error));
-  }
+                // Calculate the inCoin quantity if the outCoin price is available
+                if (outCoinPrice) {
+                    const outCoinQuantity = parseFloat(document.querySelector("#outCoin-transfer-quantity").value);
+                    const inCoinQuantity = calculateInCoinQuantity(outCoinQuantity);
+                    const quantityLabel = document.querySelector("#inCoin-quantity-label #inCoin-transfer-quantity");
+                    if (!isNaN(inCoinQuantity)) {
+                        quantityLabel.textContent = `${inCoinQuantity.toFixed(6)} ${selectedInSymbol}`;
+                    } else {
+                        quantityLabel.textContent = "";
+                    }
+                }
+            })
+            .catch(error => console.error(error));
+    }
 });
 
 // Add event listener to outCoin dropdown button
 const outDropdownBtn = document.querySelector(".outCoin-dropdown-content");
-outDropdownBtn.addEventListener("click", function(event) {
-  // Get the selected coin from the clicked dropdown item
-  if (event.target.tagName === "A") {
-    selectedOutCoin = event.target.getAttribute("data-outCoin");
-    selectedOutSymbol = event.target.getAttribute("data-outSymbol");
-    let apiUrl = `https://api.coingecko.com/api/v3/simple/price?ids=${selectedOutCoin}&vs_currencies=GBP`;
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        outCoinPrice = data[selectedOutCoin].gbp;
-        console.log(outCoinPrice); // This will print the price in GBP to the console
+outDropdownBtn.addEventListener("click", function (event) {
+    // Get the selected coin from the clicked dropdown item
+    if (event.target.tagName === "A") {
+        selectedOutCoin = event.target.getAttribute("data-outCoin");
+        selectedOutSymbol = event.target.getAttribute("data-outSymbol");
+        let apiUrl = `https://api.coingecko.com/api/v3/simple/price?ids=${selectedOutCoin}&vs_currencies=GBP`;
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                outCoinPrice = data[selectedOutCoin].gbp;
+                console.log(outCoinPrice); // This will print the price in GBP to the console
 
-        // Calculate the inCoin quantity if the inCoin price is available
-        if (inCoinPrice) {
-          const outCoinQuantity = parseFloat(document.querySelector("#outCoin-transfer-quantity").value);
-          const inCoinQuantity = calculateInCoinQuantity(outCoinQuantity);
-          const quantityLabel = document.querySelector("#inCoin-quantity-label #inCoin-transfer-quantity");
-          if (!isNaN(inCoinQuantity)) {
-            quantityLabel.textContent = `${inCoinQuantity.toFixed(6)} ${selectedInSymbol}`;
-          } else {
-            quantityLabel.textContent = "";
-          }
-        }
-      })
-      .catch(error => console.error(error));
-  }
+                // Calculate the inCoin quantity if the inCoin price is available
+                if (inCoinPrice) {
+                    const outCoinQuantity = parseFloat(document.querySelector("#outCoin-transfer-quantity").value);
+                    const inCoinQuantity = calculateInCoinQuantity(outCoinQuantity);
+                    const quantityLabel = document.querySelector("#inCoin-quantity-label #inCoin-transfer-quantity");
+                    if (!isNaN(inCoinQuantity)) {
+                        quantityLabel.textContent = `${inCoinQuantity.toFixed(6)} ${selectedInSymbol}`;
+                    } else {
+                        quantityLabel.textContent = "";
+                    }
+                }
+            })
+            .catch(error => console.error(error));
+    }
 });
 
 // Function to handle input change in the outCoin quantity and dynamically update quantity
 const outCoinInput = document.querySelector("#outCoin-transfer-quantity");
-outCoinInput.addEventListener("input", function() {
-  const outCoinQuantity = parseFloat(outCoinInput.value);
-  const inCoinQuantity = calculateInCoinQuantity(outCoinQuantity);
-  const quantityLabel = document.querySelector("#inCoin-quantity-label #inCoin-transfer-quantity");
-  if (!isNaN(inCoinQuantity)) {
-    quantityLabel.textContent = `${inCoinQuantity.toFixed(6)} ${selectedInSymbol}`;
-  } else {
-    quantityLabel.textContent = "";
-  }
+outCoinInput.addEventListener("input", function () {
+    const outCoinQuantity = parseFloat(outCoinInput.value);
+    const inCoinQuantity = calculateInCoinQuantity(outCoinQuantity);
+    const quantityLabel = document.querySelector("#inCoin-quantity-label #inCoin-transfer-quantity");
+    if (!isNaN(inCoinQuantity)) {
+        quantityLabel.textContent = `${inCoinQuantity.toFixed(6)} ${selectedInSymbol}`;
+    } else {
+        quantityLabel.textContent = "";
+    }
 });
 
-// import { ethers } from "ethers";
-// import { useState } from "react";
+import {ethers} from "ethers";
+import {useState} from "react";
+
+/**
+ *
+ */
 const [isConnected, setIsConnect] = useState(false);
+
+/**
+ * signer: To sign the transaction when interact with the contract
+ */
 const [signer, setSigner] = useState();
+
+/**
+ * The out coin address on ETH chain
+ * @type {Map<String, String>}
+ */
 const outCoinAddressMap = new Map();
 outCoinAddressMap.set("USDT", "0xdAC17F958D2ee523a2206206994597C13D831ec7");
 outCoinAddressMap.set("USDC", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48");
@@ -237,128 +258,146 @@ outCoinAddressMap.set("WBTC", "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599");
 outCoinAddressMap.set("MATIC", "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0");
 outCoinAddressMap.set("SHIB", "0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce");
 
+/**
+ * The in coin address on ETH chain
+ * @type {Map<String, String>}
+ */
 const inCoinAddressMap = new Map();
 outCoinAddressMap.set("USDT", "0xdAC17F958D2ee523a2206206994597C13D831ec7");
 outCoinAddressMap.set("USDC", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48");
 outCoinAddressMap.set("GBPT", "0x86b4dbe5d203e634a12364c0e428fa242a3fba98");
 
-let dataSymbolForCoinAddress = ""
-dropdownBtn.addEventListener("click", function (event) {
+/**
+ * To store the user selected coin type from the web page
+ * @type {string}
+ */
+let dataSymbolForCoinAddress = "";
+
+/**
+ * If the users choose to use a coin that doesn't listed on the inCoinDropdown menu,
+ * then get the value (a coin address on ETH chain) from the input label and
+ * store it here.
+ * @type {string}
+ */
+let inCoinInputAddress = "";
+
+inCoinDropdownContent.addEventListener("click", function (event) {
 
     if (event && event.target.tagName === "A") {
-        dataSymbolForCoinAddress = event.target.getAttribute("data-symbol");
+        dataSymbolForCoinAddress = event.target.getAttribute("data-inSymbol");
+        if (dataSymbolForCoinAddress === "ERC-20") {
+            // user choose to use coin that doesn't list on the dropdown menu
+            // get the input address of that coin
+            inCoinInputAddress = document.getElementById("inCoinErc20-token-address").value;
+        }
     }
 
 });
 
 // Add event listener to out coin dropdown button
+/**
+ * The out coin drop down button to add listener to get the users' selection on which
+ * coin the recipient want
+ * @type {Element}
+ */
 const outCoinDropdownBtn = document.querySelector(".outCoin-dropdown-content");
 
+/**
+ * To store the user selected coin type from the web page
+ * @type {string}
+ */
 let outCoinSymbolForAddress = "";
+
+/**
+ * If the users choose to use a coin that doesn't listed on the outCoinDropdown menu,
+ * then get the value (a coin address on ETH chain) from the input label and
+ * store it here.
+ * @type {string}
+ */
+let outCoinInputAddress = "";
+
 outCoinDropdownBtn.addEventListener("click", function (event) {
     if (event && event.target.tagName === "A") {
-        outCoinSymbolForAddress = event.target.getAttribute("data-symbol");
+        outCoinSymbolForAddress = event.target.getAttribute("data-outSymbol");
+        if (dataSymbolForCoinAddress === "ERC-20") {
+            // user choose to use coin that doesn't list on the dropdown menu
+            // get the input address of that coin
+            outCoinInputAddress = document.getElementById("outCoinErc20-token-address").value;
+        }
     }
 });
 
 /* Execute the contract */
+/**
+ * The main function to interact with the contract.
+ * @returns {Promise<void>}
+ */
 async function execute() {
     // the following are needed:
     // "outCoin": the contract address of the coin the recipient wants.
     // "outAmount": the amount of outCoin which the recipient is expecting.
     // "inCoin": the contract address of the coin the sender wants to spend.
     // "maxInAmount": the maximum amount of inCoin the user is willing to spend.
-    // "recipient": the address of the recipient who will recieve outAmount of outCoin.
+    // "recipient": the address of the recipient who will receive outAmount of outCoin.
 
     const contractAddress = ""; // the address of contract
     const abi = [
         {
-            inputs: [
+            "inputs": [
                 {
-                    internalType: "string",
-                    name: "_name",
-                    type: "string",
+                    "internalType": "address",
+                    "name": "outCoin",
+                    "type": "address"
                 },
                 {
-                    internalType: "uint256",
-                    name: "_favoriteNumber",
-                    type: "uint256",
+                    "internalType": "uint256",
+                    "name": "outAmount",
+                    "type": "uint256"
                 },
+                {
+                    "internalType": "address",
+                    "name": "recipient",
+                    "type": "address"
+                }
             ],
-            name: "addPerson",
-            outputs: [],
-            stateMutability: "nonpayable",
-            type: "function",
+            "name": "swapEthSendToken",
+            "outputs": [],
+            "stateMutability": "payable",
+            "type": "function"
         },
         {
-            inputs: [
+            "inputs": [
                 {
-                    internalType: "string",
-                    name: "",
-                    type: "string",
+                    "internalType": "address",
+                    "name": "outCoin",
+                    "type": "address"
                 },
+                {
+                    "internalType": "uint256",
+                    "name": "outAmount",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "address",
+                    "name": "inCoin",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "maxInAmount",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "address",
+                    "name": "recipient",
+                    "type": "address"
+                }
             ],
-            name: "nameToFavoriteNumber",
-            outputs: [
-                {
-                    internalType: "uint256",
-                    name: "",
-                    type: "uint256",
-                },
-            ],
-            stateMutability: "view",
-            type: "function",
-        },
-        {
-            inputs: [
-                {
-                    internalType: "uint256",
-                    name: "",
-                    type: "uint256",
-                },
-            ],
-            name: "people",
-            outputs: [
-                {
-                    internalType: "uint256",
-                    name: "favoriteNumber",
-                    type: "uint256",
-                },
-                {
-                    internalType: "string",
-                    name: "name",
-                    type: "string",
-                },
-            ],
-            stateMutability: "view",
-            type: "function",
-        },
-        {
-            inputs: [],
-            name: "retrieve",
-            outputs: [
-                {
-                    internalType: "uint256",
-                    name: "",
-                    type: "uint256",
-                },
-            ],
-            stateMutability: "view",
-            type: "function",
-        },
-        {
-            inputs: [
-                {
-                    internalType: "uint256",
-                    name: "_favoriteNumber",
-                    type: "uint256",
-                },
-            ],
-            name: "store",
-            outputs: [],
-            stateMutability: "nonpayable",
-            type: "function",
-        },
+            "name": "swapTokenSendToken",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }
     ];
     const contract = new ethers.Contract(contractAddress, abi, signer);
 
@@ -369,10 +408,11 @@ async function execute() {
                 // uint256 outAmount,
                 // address recipient
                 await contract.swapEthSendToken(
-                    outCoinAddressMap.get(outCoinSymbolForAddress),
+                    outCoinInputAddress === "" ? outCoinAddressMap.get(outCoinSymbolForAddress) : outCoinInputAddress,
                     Number(Document.getElementById("transfer-gbp-value").value),
                     Document.getElementById("recipient"));
             } catch (error) {
+                alert("Transaction failed! Please check if you input the right coin address.");
                 console.log(error);
             }
         } else {
@@ -380,14 +420,23 @@ async function execute() {
         }
     } else {
         if (typeof window.ethereum !== "undefined") {
+            let outAmount = Number(Document.getElementById("transfer-gbp-value").value);
             try {
                 //address outCoin,
                 //uint256 outAmount,
                 //address inCoin,
                 //uint256 maxInAmount,
                 //address recipient
-                await contract.swapTokenSendToken("", Number(Document.getElementById("transfer-gbp-value").value), Document.getElementById("recipient"));
+                await contract.swapTokenSendToken(
+                    // outCoin
+                    outCoinInputAddress === "" ? outCoinAddressMap.get(outCoinSymbolForAddress) : outCoinInputAddress,
+                    outAmount, // outAmount
+                    // inCoin
+                    inCoinInputAddress === "" ? inCoinAddressMap.get(dataSymbolForCoinAddress) : inCoinInputAddress,
+                    calculateInCoinQuantity(outAmount), // maxInAmount
+                    Document.getElementById("recipient")); // recipient
             } catch (error) {
+                alert("Transaction failed! Please check if you input the right coin address.");
                 console.log(error);
             }
         } else {
